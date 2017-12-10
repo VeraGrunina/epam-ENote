e91e58081e900b22b4179ccbefd633603b07bbac
VeraGrunina <vegrunina@mail.ru>
2017-12-10 17:54:26 +0300

    add AppConfig

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -1,12 +1,76 @@
 package config;
 
+import lombok.extern.slf4j.Slf4j;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.orm.hibernate4.LocalSessionFactoryBean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.io.IOException;
+import java.sql.SQLException;
 
 @Configuration
-@EnableTransactionManagement
-@ComponentScan(basePackages = "models")
+@PropertySource("database.properties")
+@EnableJpaRepositories("repositories")
+@ComponentScan(basePackages = "model")
 public class AppConfig {
+
+    @Bean
+    public DataSource h2dataSource() throws SQLException {
+        return new EmbeddedDatabaseBuilder()
+                .setName("test")
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("classpath:src/java/resources/init.sql")
+                .build();
+    }
+
+    @Bean
+    public JpaVendorAdapter jpaVendorAdapter() {
+        HibernateJpaVendorAdapter bean = new HibernateJpaVendorAdapter();
+        bean.setDatabase(Database.H2);
+        return bean;
+    }
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+        LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
+        try {
+            em.setDataSource(h2dataSource());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        em.setPackagesToScan("model");
+        em.setJpaVendorAdapter(jpaVendorAdapter());
+        return em;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+
+    @Bean(name = "sessionFactory")
+    public SessionFactory sessionFactory() throws IOException, SQLException {
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(h2dataSource());
+        sessionFactoryBean.setPackagesToScan("model");
+        sessionFactoryBean.setAnnotatedPackages("model");
+        sessionFactoryBean.afterPropertiesSet();
+        return sessionFactoryBean.getObject();
+    }
 
 }
