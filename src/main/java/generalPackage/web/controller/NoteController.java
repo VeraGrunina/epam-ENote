4c0d69ea7c96dee501aade0c7cb89a83ff1013d6
VeraGrunina <vegrunina@mail.ru>
2017-12-23 17:37:20 +0300

    change NoteController for Transformer

--- src/main/java/generalPackage/web/controller/NoteController.java
+++ src/main/java/generalPackage/web/controller/NoteController.java
@@ -1,6 +1,14 @@
 package generalPackage.web.controller;
 
-import generalPackage.data.entity.NoteWebModel;
+import generalPackage.data.entity.Note;
+import generalPackage.data.entity.Notebook;
+import generalPackage.exception.ApplicationRuntimeException;
+import generalPackage.service.interfaces.NotebookService;
+import generalPackage.web.model.NoteWebModel;
+import generalPackage.web.model.NotebookWebModel;
+import generalPackage.web.transformer.NoteTransformer;
+import generalPackage.web.transformer.NotebookTransformer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import generalPackage.service.interfaces.NoteService;
@@ -10,16 +18,27 @@ import java.util.List;
 @RestController
 @RequestMapping("/notebook/{notebookId}")
 public class NoteController {
-    private final NoteService noteService;
+    @Autowired
+    private NoteService noteService;
 
-    public NoteController(NoteService noteService) {
-        this.noteService = noteService;
-    }
+    @Autowired
+    private NotebookService notebookService;
+
+    @Autowired
+    private NoteTransformer noteTransformer;
+
+    @Autowired
+    private NotebookTransformer notebookTransformer;
 
     @PostMapping("/note")
     @ResponseStatus(HttpStatus.CREATED)
     public void create(@RequestBody NoteWebModel noteWebModel, @PathVariable("notebookId") Long notebookId) {
-        noteWebModel = noteService.createNote(noteWebModel);
+
+        Notebook notebook = notebookService.readNotebookById(notebookId);
+        Note note = noteTransformer.bind(noteWebModel);
+        note.setNotebook(notebook);
+
+        noteService.createNote(note);
 
         String url = "/" + noteWebModel.getId();
 
@@ -28,24 +47,35 @@ public class NoteController {
 
     @PutMapping("/note/{noteId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void updateNote(@PathVariable Long noteId, @RequestBody NoteWebModel noteWebModel) {
-        noteService.updateNote(noteWebModel);
+    public void updateNote(@PathVariable("noteId") Long noteId, @RequestBody NoteWebModel noteWebModel) {
+        Note note = noteTransformer.bind(noteWebModel);
+
+        if (!note.getId().equals(noteId)) {
+            throw new ApplicationRuntimeException("You can't change id");
+        }
+
+        noteService.updateNote(note);
     }
 
     @GetMapping("/note/{noteId}")
-    public NoteWebModel findNoteById(@PathVariable Long noteId) {
-        return noteService.readNoteById(noteId);
+    public NoteWebModel findNoteById(@PathVariable("noteId") Long noteId) {
+        Note note = noteService.readNoteById(noteId);
+
+        return noteTransformer.unbind(note);
     }
 
     @GetMapping("/notes")
-    public List<NoteWebModel> findAllNotes() {
-        return noteService.list();
+    public NotebookWebModel findAllNotes(@PathVariable("notebookId") Long notebookId) {
+        Notebook notebook = notebookService.readNotebookById(notebookId);
+
+        return notebookTransformer.unbind(notebook);
     }
 
     @DeleteMapping("/note/{noteId}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void removeNoteFromNotebook(@PathVariable Long noteId) {
-        noteService.deleteNote(noteId);
+        Note note = noteService.readNoteById(noteId);
+        noteService.deleteNote(note);
     }
 
 
